ae913646971d0906d49bbd481d29548b76e859d9
Pallavinehaaa23 <114822231+Pallavinehaaa23@users.noreply.github.com>
2023-06-17 19:46:56 +0530

    Update cocur.js

--- js/cocur.js
+++ js/cocur.js
@@ -67,4 +67,17 @@ const postmethods =()=>{
        postcontainer.appendChild(postelement);
      })
 }
-postmethods()
+postmethods
+window.addEventListener('scroll',reveal);
+function reveal(){
+   var reveals=document.querySelectorAll('.reveal');
+ for(i=0;i<reveals.length;i++){
+   var windowheight=window.innerHeight;
+   var revealTop= reveals[i].getBoundingClientRect().top;
+   var revealpoint =150;
+   if(revealTop<windowheight-revealpoint)
+   reveals[i].classList.add('active');
+   else
+   reveals[i].classList.remove('active');
+ }
+}    
